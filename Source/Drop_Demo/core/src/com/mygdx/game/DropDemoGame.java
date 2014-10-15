3dbd8978d24a53d00bca42dd8781a00ae2a5f110
Yacine Mekesser <ymekesser@hotmail.com>
2014-10-15 20:02:40 +0200

    Habe das "Drop"-Beispiel vom libGDX Wiki mal als ausführbares Projekt erstellt.

---
+++ Source/Drop_Demo/core/src/com/mygdx/game/DropDemoGame.java
@@ -0,0 +1,29 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class DropDemoGame extends Game {
+
+	SpriteBatch batch;
+	BitmapFont font;
+
+	public void create() {
+		System.out.println("Create DropDemoGame");
+		batch = new SpriteBatch();
+		// Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	public void render() {
+		super.render(); // important!
+	}
+
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+
+}
