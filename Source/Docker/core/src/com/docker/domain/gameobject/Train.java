72ae596330a684547ca938cc7b7bb510731cd4df
Emily <em.wangler@gmail.com>
2014-11-04 10:09:14 +0100

    zug rendering

--- Source/Docker/core/src/com/docker/domain/gameobject/Train.java
+++ Source/Docker/core/src/com/docker/domain/gameobject/Train.java
@@ -1,37 +1,36 @@
 package com.docker.domain.gameobject;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Train extends Actor {
 	private static final float PADDING = 11;
-	
+
 	private int speed;
-	private List<Container> containers;
-	
+	private Queue<Container> containers;
+
 	private TextureRegion platform_single;
 	private TextureRegion platform_center;
 	private TextureRegion platform_left;
 	private TextureRegion platform_right;
 	private TextureRegion wheel;
-	
+
 	public Train(int speed, float x, float y) {
 		super();
 		this.setX(x);
 		this.setY(y);
 		this.speed = speed;
 		this.containers = new LinkedList<Container>();
-		
+
 		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("img/docker.atlas"));
 		this.platform_single = atlas.findRegion("train_platform_single");
 		this.platform_center = atlas.findRegion("train_platform_center");
@@ -39,26 +38,47 @@ public class Train extends Actor {
 		this.platform_right = atlas.findRegion("train_platform_right");
 		this.wheel = atlas.findRegion("train_wheel");
 	}
-	
+
 	public void addContainer(Container container){
 		this.containers.add(container);
 	}
-	
+
 	public Container popContainer(){
-		throw new NotImplementedException();
+		return containers.remove();
 	}
-	
+	public Container getContainer(){
+		return containers.peek();
+	}
+
 	@Override
 	public void act(float delta){
-		float xPos = this.getX();
+        List<Container> toRemove = new ArrayList<Container>();
+        float lastX = 0f;
 		for (Container container : this.containers) {
-			container.setX(xPos);
-			container.setY(this.getY());
-			xPos += container.getWidth() + this.PADDING;
-		}
-	}
-	
-	@Override
+            float xPos = container.getX();
+
+            if (lastX == 0 || fits(container, lastX) || !toRemove.isEmpty()) {
+                container.setY(this.getY());
+                if (xPos + container.getWidth() < 600) {
+                    xPos = speed + container.getX();
+                    container.setX(xPos);
+                    lastX = xPos;
+                } else {
+                    toRemove.add(container);
+                }
+            } else {
+                container.setX(container.getWidth()*-1);
+            }
+        }
+        if (!toRemove.isEmpty()) containers.removeAll(toRemove);
+        // leben abziehen
+    }
+
+    private boolean fits(Container container, float lastX) {
+        return container.getWidth() + container.getX() + PADDING < lastX;
+    }
+
+    @Override
 	public void draw (Batch batch, float parentAlpha) {
 		for (Container container : this.containers) {
 			batch.draw(
@@ -69,28 +89,27 @@ public class Train extends Actor {
 					this.wheel,
 					container.getX() + container.getWidth() - this.wheel.getRegionWidth(),
 					container.getY() - 8);
-			
+
 			container.draw(batch, parentAlpha);
-			
+
 			if(container.getLength() == 1){
 				batch.draw(
 						this.platform_single,
 						container.getX() - 6f,
 						container.getY() - 4f);
-			}
-			else{
+			} else {
 				float elementWidth = container.getElementWidth();
 				batch.draw(
 						this.platform_left,
 						container.getX() - 6f,
 						container.getY() - 4f);
 				for (int i = 1; i <= container.getLength()-2; i++) {
-					batch.draw(this.platform_center, 
-							container.getX() + elementWidth*i, 
-							container.getY() - 4f);			
+					batch.draw(this.platform_center,
+							container.getX() + elementWidth*i,
+							container.getY() - 4f);
 				}
-				batch.draw(this.platform_right, 
-						container.getX() + elementWidth*(container.getLength()-1) - 3, 
+				batch.draw(this.platform_right,
+						container.getX() + elementWidth*(container.getLength()-1) - 3,
 						container.getY() - 4f);
 			}
 		}
