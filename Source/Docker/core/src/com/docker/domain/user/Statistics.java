f16139a77d16c21c1eb1c1bb8560445f9591aa9a
Emily <em.wangler@gmail.com>
2014-11-10 21:19:39 +0100

    persist funktionen für statistik hinzugefügt

--- Source/Docker/core/src/com/docker/domain/user/Statistics.java
+++ Source/Docker/core/src/com/docker/domain/user/Statistics.java
@@ -1,5 +1,11 @@
 package com.docker.domain.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.utils.ObjectMap;
+import com.docker.technicalservices.Persistence;
+
 public class Statistics {
 	private int totalScore;
 	private int totalContainer;
@@ -9,8 +15,7 @@ public class Statistics {
 	private int totalShipsCapsized;
 	private int totalShipsBroken;
 	
-	//wie wird die Statistik persistent abgelegt und wieder geladen?
-	public Statistics(){
+	public Statistics() {
 		this.totalScore = 0;
 		this.totalContainer = 0;
 		this.totalWeight = 0;
@@ -19,8 +24,19 @@ public class Statistics {
 		this.totalShipsCapsized = 0;
 		this.totalShipsBroken = 0;
 	}
-	
-	public int getTotalScore() {
+
+    public Statistics(int totalScore, int totalContainer, int totalWeight, int totalGames,
+                      int totalShipsSuccessfullyLoaded, int totalShipsCapsized, int totalShipsBroken) {
+        this.totalScore = totalScore;
+        this.totalContainer = totalContainer;
+        this.totalWeight = totalWeight;
+        this.totalGames = totalGames;
+        this.totalShipsSuccessfullyLoaded = totalShipsSuccessfullyLoaded;
+        this.totalShipsCapsized = totalShipsCapsized;
+        this.totalShipsBroken = totalShipsBroken;
+    }
+
+    public int getTotalScore() {
 		return totalScore;
 	}
 	public int getTotalContainer() {
@@ -62,4 +78,39 @@ public class Statistics {
 	public void incrementTotalShipsBroken(int totalShipsBroken) {
 		this.totalShipsBroken += totalShipsBroken;
 	}
+
+    public void persistTotalScore(int totalScore, Persistence persistence) {
+        persistence.saveStatisticValue("totalScore", totalScore);
+    }
+    public void persistTotalContainer(int totalContainer, Persistence persistence) {
+        persistence.saveStatisticValue("totalContainer", totalContainer);
+    }
+    public void persistTotalWeight(int totalWeight, Persistence persistence) {
+        persistence.saveStatisticValue("totalWeight", totalWeight);
+    }
+    public void persistTotalGames(int totalGames, Persistence persistence) {
+        persistence.saveStatisticValue("totalGames", totalGames);
+    }
+    public void persistTotalShipsSuccessfullyLoaded(int totalShipsSuccessfullyLoaded, Persistence persistence) {
+        persistence.saveStatisticValue("totalShipsSuccessfullyLoaded", totalShipsSuccessfullyLoaded);
+    }
+    public void persistTotalShipsCapsized(int totalShipsCapsized, Persistence persistence) {
+        persistence.saveStatisticValue("totalShipsCapsized", totalShipsCapsized);
+    }
+    public void persistTotalShipsBroken(int totalShipsBroken, Persistence persistence) {
+        persistence.saveStatisticValue("totalShipsBroken", totalShipsBroken);
+    }
+
+    public void persistStatistics(Persistence persistence, Statistics statistics) {
+        ObjectMap<String, Object> map = new ObjectMap<String, Object>();
+        map.put("totalScore", statistics.getTotalScore());
+        map.put("totalContainer", statistics.getTotalContainer());
+        map.put("totalWeight", statistics.getTotalWeight());
+        map.put("totalGames", statistics.getTotalGames());
+        map.put("totalShipsSuccessfullyLoaded", statistics.getTotalShipsSuccessfullyLoaded());
+        map.put("totalShipsCapsized", statistics.getTotalShipsCapsized());
+        map.put("totalShipsBroken", statistics.getTotalShipsBroken());
+        persistence.writeStatisticMap(map);
+    }
+
 }
