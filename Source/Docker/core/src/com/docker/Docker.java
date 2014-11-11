4e52c6409e87037cd59dd17de448cb304225d2d6
Emily <em.wangler@gmail.com>
2014-11-11 11:15:53 +0100

    persistenz im docker initialisieren

--- Source/Docker/core/src/com/docker/Docker.java
+++ Source/Docker/core/src/com/docker/Docker.java
@@ -2,14 +2,17 @@ package com.docker;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.docker.technicalservices.Persistence;
 import com.docker.ui.menus.MainMenu;
 
 public class Docker extends Game {
 	Screen lastScreen;
+    public Persistence persistence;
 	
 	@Override
 	public void create() {
 		this.setScreen(new MainMenu(this));
+        this.persistence = new Persistence();
 	}
 	
 	@Override
