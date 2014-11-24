49acc5b57657f39d9c91596857e54516ea9659ad
Yacine Mekesser <ymekesser@hotmail.com>
2014-11-24 14:38:35 +0100

    setLastScreen() funktioniert jetzt über mehrere Hierarchiestufen hinweg.
    lastScreen ist nun ein Stack und nicht mehr nur eine Variable.

--- Source/Docker/core/src/com/docker/Docker.java
+++ Source/Docker/core/src/com/docker/Docker.java
@@ -1,5 +1,7 @@
 package com.docker;
 
+import java.util.Stack;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.docker.technicalservices.Persistence;
@@ -9,22 +11,26 @@ public class Docker extends Game {
 	public static final float WIDTH = 360;
 	public static final float HEIGHT = 200;
 	
-	Screen lastScreen;
+	Stack<Screen> history;
     public Persistence persistence;
 	
 	@Override
 	public void create() {
+		this.history = new Stack<Screen>();
 		this.setScreen(new MainMenu(this));
         this.persistence = new Persistence();
 	}
 	
 	@Override
 	public void setScreen(Screen screen){
-		this.lastScreen = this.getScreen();
+		if(this.getScreen() != null){
+			history.push(this.getScreen());
+		}
 		super.setScreen(screen);
 	}
 	
 	public void setLastScreen(){
-		this.setScreen(this.lastScreen);
+		if(history.size() > 0)
+			super.setScreen(history.pop());
 	}
 }
