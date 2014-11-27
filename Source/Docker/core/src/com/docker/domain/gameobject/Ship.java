5aaf2642c7a65fc139dee8fdd916f68000bf307e
it-monkey <remo.hoeppli@gmail.com>
2014-11-27 21:34:52 +0100

    Java Doc and Refactoring

--- Source/Docker/core/src/com/docker/domain/gameobject/Ship.java
+++ Source/Docker/core/src/com/docker/domain/gameobject/Ship.java
@@ -2,7 +2,6 @@ package com.docker.domain.gameobject;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
--- Source/Docker/core/src/com/docker/domain/gameobject/Train.java
+++ Source/Docker/core/src/com/docker/domain/gameobject/Train.java
@@ -1,7 +1,6 @@
 package com.docker.domain.gameobject;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
