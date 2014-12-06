d823543d62bce55434b0aac6c317c4907dcff788
Yacine Mekesser <ymekesser@hotmail.com>
2014-12-06 12:19:24 +0100

    Endscreen verschönert. Fixes #36

--- Source/Docker/core/src/com/docker/ui/menus/EndScreen.java
+++ Source/Docker/core/src/com/docker/ui/menus/EndScreen.java
@@ -10,7 +10,7 @@ import com.docker.Docker;
 public class EndScreen extends AbstractMenu {
 
 	private TextButton endGameButton = new TextButton("Exit", skin);
-    private Label title = new Label("The Game Is Over!!!",skin);
+    private Label title = new Label("Game Over",skin,"title");
     
 
     public EndScreen(final Docker application, TextureRegion background, int score, int highscore) {
@@ -26,7 +26,7 @@ public class EndScreen extends AbstractMenu {
         table.add(new Label("Your Score:", skin)).width(120).left();
         table.add(new Label(""+score+"", skin)).width(70).row();
         table.add(new Label("Your Highscore:", skin)).width(120).left();
-        table.add(new Label(""+highscore+"", skin)).width(70);
+        table.add(new Label(""+highscore+"", skin)).width(70).padBottom(20).row();
         table.add(endGameButton).size(100, 30).left().row();
 	}
     
@@ -39,8 +39,8 @@ public class EndScreen extends AbstractMenu {
                 application.returnToMainmenu();
             }
         });
-        table.add(title).left().padBottom(15).row().padBottom(10);
-        table.add(new Label("You Lose!", skin)).width(120).left();
+        table.add(title).padBottom(15).row().padBottom(10);
+        table.add(new Label("You lose!", skin)).width(120).left().row();
         table.add(endGameButton).size(100, 30).left().row();
 	}
 }
