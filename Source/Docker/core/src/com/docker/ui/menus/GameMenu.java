6be406cb22c8a22af8efded50c8c52077a30a5ac
Yacine Mekesser <ymekesser@hotmail.com>
2015-03-03 09:23:11 +0100

    Button für Tutorial ins Game Menu eingebunden (fixex #58)

--- Source/Docker/core/src/com/docker/ui/menus/GameMenu.java
+++ Source/Docker/core/src/com/docker/ui/menus/GameMenu.java
@@ -13,57 +13,66 @@ import com.docker.domain.game.TutorialGame;
  * In this menu the player can choose which game he wants to play
  */
 public class GameMenu extends AbstractMenu {
-    private Label title = new Label("Game Modes",skin, "title");
-    private TextButton careerGameButton = new TextButton("Career Game", skin);
-    private TextButton quickGameButton = new TextButton("Quick Game", skin);
-    private TextButton infiniteGameButton = new TextButton("Tutorial Test", skin);
-    private TextButton backButton = new TextButton("Back", skin);
+	private Label title = new Label("Game Modes",skin, "title");
+	private TextButton careerGameButton = new TextButton("Career Game", skin);
+	private TextButton quickGameButton = new TextButton("Quick Game", skin);
+	private TextButton infiniteGameButton = new TextButton("Infinite Game", skin);
+	private TextButton tutorialGameButton = new TextButton("How to", skin);
+	private TextButton backButton = new TextButton("Back", skin);
 
 
-    /**
-     * @param application A reference to the Docker Application (Game) object.
-     */
-    public GameMenu(final Docker application) {
-        super(application);
-        
-        backButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                application.returnToLastScreen();
-            }
-        });
-        careerGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                openNewMenu(new CareerMenu(application));
-            }
-        });
-        quickGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                application.showAds(false);
-                application.setScreen(new QuickGame(application));
-            }
-        });
-        infiniteGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                application.showAds(false);
-                application.setScreen(new InfiniteGame(application));
-            }
-        });
-        infiniteGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                application.showAds(false);
-                application.setScreen(new TutorialGame(application));
-            }
-        });
+	/**
+	 * @param application A reference to the Docker Application (Game) object.
+	 */
+	public GameMenu(final Docker application) {
+		super(application);
 
-        table.add(title).padBottom(10).row();
-        table.add(careerGameButton).size(160, 35).padBottom(5).row();
-        table.add(quickGameButton).size(160, 35).padBottom(5).row();
-        table.add(infiniteGameButton).size(160, 35).padBottom(5).row();
-        table.add(backButton).size(100, 30).center();
-    }
+		backButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				application.returnToLastScreen();
+			}
+		});
+		careerGameButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				openNewMenu(new CareerMenu(application));
+			}
+		});
+		quickGameButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				application.showAds(false);
+				application.setScreen(new QuickGame(application));
+			}
+		});
+		infiniteGameButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				application.showAds(false);
+				application.setScreen(new InfiniteGame(application));
+			}
+		});
+		infiniteGameButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				application.showAds(false);
+				application.setScreen(new InfiniteGame(application));
+			}
+		});
+		tutorialGameButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				application.showAds(false);
+				application.setScreen(new TutorialGame(application));
+			}
+		});
+		tutorialGameButton.setColor(0.5f, 1f, 0.5f, 1f);
+		table.add(title).center().colspan(2).padBottom(10).row();
+		table.add(careerGameButton).size(160, 35).center().colspan(2).padBottom(5).row();
+		table.add(quickGameButton).size(160, 35).center().colspan(2).padBottom(5).row();
+		table.add(infiniteGameButton).size(160, 35).center().colspan(2).padBottom(5).row();
+		table.add(backButton).size(100, 30).padRight(5);
+		table.add(tutorialGameButton).size(100, 30).right();
+	}
 }
